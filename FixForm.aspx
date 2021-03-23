7d33b609c7679e06b809ccab63a2f1974a405b95
瑋芝 張 <juice121457@gmail.com>
2021-03-23 16:56:36 +0800

    加入專案檔案。

---
+++ FixForm.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FixForm.aspx.cs" Inherits="HomeWork002.FixForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+     
+ 
+        <%--用Div標籤包住span跟textbox,作為輸出欄位,做出欄為標題--%>
+        <div>
+            <span>sid:</span>
+            <asp:TextBox ID="TextSid" runat="server"></asp:TextBox>
+
+        </div>
+          <div>
+            <span>設備名稱:</span>
+            <asp:TextBox ID="Textequipment_name" runat="server"></asp:TextBox>
+
+        </div>
+          <div>
+            <span>送修時間:</span>
+            <asp:TextBox ID="Textrestoration_time" runat="server"></asp:TextBox>
+
+        </div>
+          <div>
+            <span>故障日期:</span>
+            <asp:TextBox ID="Textfailure_date" runat="server"></asp:TextBox>
+
+        </div>
+          <div>
+            <span>維修廠商:</span>
+            <asp:TextBox ID="Textrestoration_factory" runat="server"></asp:TextBox>
+
+        </div>
+          <div>
+            <span>故障原因:</span>
+            <asp:TextBox ID="Textfailure_cause" runat="server"></asp:TextBox>
+
+        </div>
+          <div>
+            <span>更換部件:</span>
+            <asp:TextBox ID="Textreplacement_department" runat="server"></asp:TextBox>
+
+        </div>
+          <div>
+            <span>備註:</span>
+            <asp:TextBox ID="Textnote" runat="server"></asp:TextBox>
+
+        </div>
+        <%--新增按鈕匿名ID--%>
+        <asp:Button ID="btnCreate" runat="server" Text="新增" OnClick="btnCreate_Click" />
+        <asp:Button ID="btnUpdate" runat="server" Text="修改" OnClick="btnUpdate_Click"/>
+        <asp:Button ID="btnDelete" runat="server" Text="刪除" OnClick="btnDelete_Click"/>
+        <asp:Button ID="btnSingleRead" runat="server" Text="單筆查詢" OnClick="Single_Click" />
+        <asp:Button ID="btnReadAll" runat="server" Text="顯示所有"  OnClick="ReadAll_Click"/>
+        <asp:Button ID="btnChange" runat="server" Text="查詢其他" OnClick="Chang_Click"/>
+
+    
+
+
+        <%--用GridView來顯示資料表--%>
+         <asp:GridView runat="server" ID="GridView1"></asp:GridView>
+    </form>
+</body>
+</html>
---
+++ FixForm.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HomeWork002
+{
+    public partial class FixForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                this.GridView1.DataSource = connetDB.FixDataTable();
+                this.GridView1.DataBind();
+            }
+            
+        } //讀取
+
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+          
+            //建立變數已接取TextBox的值
+            int sid = Convert.ToInt32(this.TextSid.Text);
+            string name = this.Textequipment_name.Text;
+            string restoration_time = this.Textrestoration_time.Text;
+            string failure_date = this.Textfailure_date.Text;
+            string restoration_factory = this.Textrestoration_factory.Text;
+            string failure_causet = this.Textfailure_cause.Text;
+            string replacement_department = this.Textreplacement_department.Text;
+            string note = this.Textnote.Text;
+
+            connetDB.CreateFix(sid, name, restoration_time, failure_date, restoration_factory,
+            failure_causet, replacement_department, note);
+
+            this.GridView1.DataSource = connetDB.FixDataTable();
+            this.GridView1.DataBind();
+
+        } //新增
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //建立變數已接取TextBox的值
+            int sid = Convert.ToInt32(this.TextSid.Text);
+            string name = this.Textequipment_name.Text;
+            string restoration_time = this.Textrestoration_time.Text;
+            string failure_date = this.Textfailure_date.Text;
+            string restoration_factory = this.Textrestoration_factory.Text;
+            string failure_causet = this.Textfailure_cause.Text;
+            string replacement_department = this.Textreplacement_department.Text;
+            string note = this.Textnote.Text;
+
+            connetDB.UpdateFix(sid, name, restoration_time, failure_date, restoration_factory,
+            failure_causet, replacement_department, note);
+
+            this.GridView1.DataSource = connetDB.FixDataTable();
+            this.GridView1.DataBind();
+
+        }  //修改
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            int sid = Convert.ToInt32(this.TextSid.Text);
+
+            connetDB.DeleteFix(sid);
+
+            this.GridView1.DataSource = connetDB.FixDataTable();
+            this.GridView1.DataBind();
+        }   //刪除單筆
+
+
+        protected void Single_Click(object sender, EventArgs e)
+        {
+            //建立變數以連接TextBox的值
+            int sid = Convert.ToInt32(this.TextSid.Text);
+            //將變數傳進Method
+            this.GridView1.DataSource = connetDB.ReadSingleFix(sid);
+            //將回傳值放進GridView
+            this.GridView1.DataBind();
+            
+        }  //讀取單筆
+
+        protected void ReadAll_Click(object sender, EventArgs e)
+        {
+            //將變數傳進Method
+            this.GridView1.DataSource = connetDB.ReadAllFix();
+            //將回傳值放進GridView
+            this.GridView1.DataBind();
+        }  //讀取全部
+
+        protected void Chang_Click(object sender, EventArgs e)  //查詢其他
+        {
+            //轉跳至其他頁面
+            Response.Redirect("https://www.w3schools.com/sql/trysql.asp?filename=trysql_delete");
+        }
+    }
+}
